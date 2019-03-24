1f8a5cff8b1220b65327e03b712f80fb60942525
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-03-24 16:51:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  smallest_value = Float.new
+  name_hash.collect do |key, value|
+    
+    
+    
 
 end
