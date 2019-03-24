01e9f03724509b649aa8c40a39de46e9164f8b34
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-03-24 17:13:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 # ikea = {:chair => 25, :table => 85, :mattress => 450}
 # key_for_min_value(ikea)
 # # => :chair
-#  
+#
 # veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
 # key_for_min_value(veggies)
 # # => "apple"
